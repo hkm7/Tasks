707b559a6ae1055976bc87b9b0fc8999f59fd158
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-04-07 17:18:01 +0530

    Early build stages

--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -3,17 +3,34 @@ var loginFlag = 0;
 
 function loginPage(){
     document.getElementById('mainBody').innerHTML = "Login Page";
+    document.getElementById('homeBtn').innerHTML = "<a class='nav-link' aria-current='page' href='javascript:homePage()'>Home</a>";
+    document.getElementById('taskList').innerHTML = "<a class='nav-link' href='javascript:taskPage()'>Task list</a>";
+    document.getElementById('loginBtn').innerHTML = "<a class='nav-link active' href='javascript:loginPage()'>Login</a>";
 }
 
 function homePage(){
     document.getElementById('mainBody').innerHTML = "Home page";
+    document.getElementById('homeBtn').innerHTML = "<a class='nav-link active' aria-current='page' href='javascript:homePage()'>Home</a>";
+    document.getElementById('taskList').innerHTML = "<a class='nav-link' href='javascript:taskPage()'>Task list</a>";
+    document.getElementById('loginBtn').innerHTML = "<a class='nav-link' href='javascript:loginPage()'>Login</a>";
 }
 
 function taskPage(){
+    document.getElementById('homeBtn').innerHTML = "<a class='nav-link' aria-current='page' href='javascript:homePage()'>Home</a>";
+    document.getElementById('taskList').innerHTML = "<a class='nav-link active' href='javascript:taskPage()'>Task list</a>";
+    document.getElementById('loginBtn').innerHTML = "<a class='nav-link' href='javascript:loginPage()'>Login</a>";
     if(loginFlag == 0){
         document.getElementById('mainBody').innerHTML = "<center>You can't access this page because you are not logged in. <br> Please login first.</center>";
     }
     else if(loginFlag==1){
-    document.getElementById('mainBody').innerHTML = "Task page";
+        document.getElementById('mainBody').innerHTML = "Task page";
     }
+}
+
+function Login(){
+
+}
+
+function Logout(){
+    loginFlag = 0;
 }
